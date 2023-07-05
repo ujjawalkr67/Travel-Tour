45e0f60d9c5d50aa120f4d089bdccde5401c9be0
Ujjawal kumar <ujjawalkr67@gmail.com>
2023-07-05 12:11:01 +0530

    new commit

--- src/Components/Home/home.jsx
+++ src/Components/Home/home.jsx
@@ -1,6 +1,16 @@
 import React from "react";
 import './home.css'
 import video from '../../assets/video.mp4'
+import {GrLocation} from 'react-icons/gr'
+import {HiFilter} from 'react-icons/hi'
+import {FiFacebook} from 'react-icons/fi'
+import {AiOutlineInstagram} from 'react-icons/ai'
+import {SiTripadvisor} from 'react-icons/si'
+import {BsListTask} from 'react-icons/bs'
+import {TbApps} from 'react-icons/tb'
+
+
+
 const Home = ()=>{
     return (
         <section className="home">
@@ -18,6 +28,60 @@ const Home = ()=>{
                         Search Your Holiday
                     </h1>
                 </div>
+
+               <div className="cardDiv grid">
+                <div className="destinationInput">
+                    <label htmlFor="city">Search Your Destination</label>
+                    <div className="input flex">
+                        <input type="text" placeholder="Enter name here"/>
+                        <GrLocation className="icon"/>
+                    </div>
+                </div>
+                <div className="destinationInput">
+                    <label htmlFor="city">Search Your Destination</label>
+                    <div className="input flex">
+                        <input type="text" placeholder="Enter name here"/>
+                        <GrLocation className="icon"/>
+                    </div>
+                </div>
+                <div className="dateInput">
+                    <label htmlFor="date">Select Your date:</label>
+                    <div className="input flex">
+                        <input type="date" />
+                    </div>
+                </div>
+                
+                <div className="priceInput">
+                   <div className="label_total flex">
+                    <label htmlFor="price">Max price:</label>
+                    <h3 className="total">$5000</h3>
+                   </div>
+                   <div className="input flex">
+                    <input type="range" max="5000" min="1000" />
+                   </div>
+                </div>
+            
+                <div className="searchOption flex">
+                    <HiFilter className="icon" />
+                    <span>MORE FILTER</span>
+                </div>
+                
+                </div>  
+
+                <div className="homeFooterIcons flex">
+                    <div className="rightIcons">
+                        <FiFacebook className="icon" />
+                        <AiOutlineInstagram className="icon" />
+                        <SiTripadvisor className="icon"/>
+                    </div>
+                    <div className="leftIcons">
+                        <BsListTask className="icon"/>
+                        <TbApps className="icon"/>
+
+                    </div>
+                </div>
+
+
             </div>
         </section>
     )
